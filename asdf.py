49b29c3680f517b1711a84f1864f8e3e250cc867
Mok Jun Neng <Mok Jun Neng>
2017-04-17 01:19:43 +0800

    asfdfasdfasdfasdf

--- asdf.py
+++ asdf.py
@@ -1,19 +1,15 @@
 import spidev
 import time
 
-
-spi = spidev.SpiDev() # create spi object
+spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1000000
 try:
     while True:
-        resp = spi.xfer2([0xAA]) # transfer one byte
+        resp = spi.xfer2([0xAA])
         print spi.readbytes(8)
-        time.sleep(0.1) # sleep for 0.1 seconds
-        #end while
-except KeyboardInterrupt: # Ctrl+C pressed, so…
- spi.close() # … close the port before exit
-#end try
-
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    spi.close()
 
 spi.xfer([value_8bit])
